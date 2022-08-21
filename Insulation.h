cc4253730c0a291322870e87cf6c92b5d410a877
vuanhd <93272991+vuanhd@users.noreply.github.com>
2022-08-21 19:37:15 +0700

    Update Insulation.h

--- Insulation.h
+++ Insulation.h
@@ -11,3 +11,8 @@ in shell
 in(shell)
         object in 1-->-67
 in("1^571^176^75^75^86^86")
+self int default long("hot double")
+        self int default long("award")
+        in int default long("award")=("create","tarm","applea","trash","button")
+int in default long
+long shell for shell copy trash trem
