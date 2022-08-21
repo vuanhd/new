dbe772a9713c17f90a150249cfd2a71b2e9fa79d
vuanhd <93272991+vuanhd@users.noreply.github.com>
2022-08-21 16:28:46 +0700

    Create Insulation.h

---
+++ Insulation.h
@@ -0,0 +1,13 @@
+//source local.h
+#include <stdio.h>
+for(i=1;i<=10;i++) i++;
+for(award)
+        for(i=1;i<=10;i++) i++;
+            10:10=10
+                    for(for(i=1;i<=10;i++) i++;)
+                            for(i=76;i>=10;i++) i++;
+for shell
+in shell
+in(shell)
+        object in 1-->-67
+in("1^571^176^75^75^86^86")
